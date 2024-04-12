34c13219b973d4233d57429fd0085dd863aac171
Duy Tran <duytran_d@yahoo.com>
2024-04-12 16:22:40 -0500

    addingshit

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,3 +1,4 @@
-const baseUrl = "http://duywebapp2.azurewebsites.net";
+// const baseUrl = "http://duywebapp2.azurewebsites.net";
+const baseUrl = "https://duywebapp2.azurewebsites.net";
 
 export default baseUrl;
