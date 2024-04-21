663b4cea90ef3080c8a1755b2ddf79c53542d39c
Duy Tran <duytran_d@yahoo.com>
2024-04-21 10:46:56 -0500

    changes to be committed

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://duywebapp2.azurewebsites.net";
+const baseUrl = "https://duywebapp2.azurewebsites.net";
 // const baseUrl = "http://localhost:3001";
 
 export default baseUrl;
